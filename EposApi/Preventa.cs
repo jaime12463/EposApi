093489e7ea9a95ded358d54175b254a6c585d80a
HSMX-DES-JGARCIAPINE <HSMX-DES-JGARCIAPINE@DESKTOP-BKASNCL>
2023-06-26 15:48:36 -0600

    Agregar archivos de proyecto.

---
+++ EposApi/Preventa.cs
@@ -0,0 +1,140 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonConverter = Newtonsoft.Json.JsonConverter;
+
+namespace EposApi
+{
+    public class Preventa
+    {
+         public Preventa(Comprobante comproba, Cajero caje, Descuento descuento, DatosCliente cliente, List<Plus> productos,string text)
+        {
+            Descuento = new List<Descuento>();
+            Comprobante = comproba;
+            Cajero = caje;
+            Descuento.Add ( descuento);
+            DatosCliente = cliente;
+            Plus = productos;
+        }
+
+        [JsonProperty("comprobante")]
+        public Comprobante Comprobante { get; set; }
+
+        [JsonProperty("cajero")]
+        public Cajero Cajero { get; set; }
+
+        [JsonProperty("descuentos")]
+        public List<Descuento> Descuento { get; set; }
+
+        [JsonProperty("datosCliente")]
+        public DatosCliente DatosCliente { get; set; }
+
+        [JsonProperty("plus")]
+        public List<Plus> Plus { get; set; }
+
+    }
+
+
+    public partial class Cajero
+    {
+        [JsonProperty("nombre")]
+        public string Nombre { get; set; }
+
+        [JsonProperty("legajo")]
+        [Newtonsoft.Json.JsonConverter(typeof(ParseStringConverter))]
+        public long Legajo { get; set; }
+    }
+
+    public partial class Comprobante
+    {
+        [JsonProperty("tipo")]
+        public string Tipo { get; set; }
+    }
+
+    public partial class DatosCliente
+    {
+        [JsonProperty("tipoDocumento")]
+        public string TipoDocumento { get; set; }
+
+        [JsonProperty("numero")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Numero { get; set; }
+
+        [JsonProperty("nombre")]
+        public string Nombre { get; set; }
+
+        [JsonProperty("direccion")]
+        public string Direccion { get; set; }
+
+        [JsonProperty("localidad")]
+        public string Localidad { get; set; }
+
+        [JsonProperty("provincia")]
+        public string Provincia { get; set; }
+
+        [JsonProperty("respAnteIva")]
+        public string RespAnteIva { get; set; }
+    }
+
+    public partial class Descuento
+    {
+        [JsonProperty("descripcion")]
+        public string Descripcion { get; set; }
+
+        [JsonProperty("porcentaje")]
+        public long Porcentaje { get; set; }
+
+        [JsonProperty("subfamilia")]
+        public long Subfamilia { get; set; }
+    }
+
+    public  class Plus
+    {
+        public Plus(string codigo ,long cantidad)
+        {
+            Codigo = codigo;
+            Cantidad = cantidad;
+        }
+        [JsonProperty("codigo")]
+        public string Codigo { get; set; }
+
+        [JsonProperty("cantidad")]
+        public long Cantidad { get; set; }
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+
+}
---
+++ EposApi/Program.cs
@@ -0,0 +1,89 @@
+﻿
+using EposApi;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+
+
+Comprobante comprobante = new Comprobante();
+Cajero cajero = new Cajero();
+Descuento descuento = new Descuento();
+DatosCliente cliente = new DatosCliente();
+
+List<Plus> productos = new List<Plus>(); 
+
+
+cajero.Nombre = "Cajero 1";
+cajero.Legajo = 1234;
+
+comprobante.Tipo = "Factura";
+
+descuento.Subfamilia = 99; // Checar de donde viene la familia de descuentos 
+descuento.Descripcion = "Descuento Test Hasar ";
+descuento.Porcentaje = (long)-10.00;
+
+cliente.TipoDocumento = "DNI";
+cliente.Direccion = "1Florencio Terrada 2076";
+cliente.Nombre = "fABIAN BARCA ";
+cliente.Numero = 223872378;
+cliente.Provincia = "CABA";
+cliente.Localidad = "Villa del Parque";
+cliente.RespAnteIva = "ConsFinal";
+
+productos.Add(new Plus("779279800773", 5));
+productos.Add(new Plus("779089500099", 2));
+productos.Add(new Plus("36", 1));
+
+
+
+string api = "http://10.0.2.62:8081/PreVenta";
+
+Uri u = new Uri(api);
+
+Preventa pre = new Preventa(comprobante, cajero, descuento, cliente, productos, "");
+
+string json = JsonConvert.SerializeObject(pre);
+
+Console.WriteLine(json);
+var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+//httpContent.Headers.ContentType=new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+//httpContent.Headers.Remove("Date");
+HttpClient httpClient = new HttpClient  {
+                Timeout = new TimeSpan(0, 0, 60)
+            };
+//httpClient.DefaultRequestHeaders.Accept.Clear();
+//httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+//httpClient.DefaultRequestHeaders.
+//var postResponse = await httpClient.PostAsync(u, httpContent);
+
+RespuestaPreventa respuesta = new RespuestaPreventa();
+
+try
+{
+    var postResponse = await  httpClient.PostAsync(u, httpContent);
+    Console.WriteLine("******************************************************");
+    Console.WriteLine("Respuesta del servidor {0}",u.ToString());
+    Console.WriteLine("");
+    Console.WriteLine("");
+    string result = postResponse.Content.ReadAsStringAsync().Result;
+
+    var res = JsonConvert.DeserializeObject<RespuestaPreventa>(result);
+
+    Console.WriteLine(JsonConvert.SerializeObject(res));
+    Console.WriteLine("*******************************************************");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());   
+}
+
+
+//postResponse.EnsureSuccessStatusCode();
+
+Console.WriteLine("Presione una tecla para salir");
+Console.Read();
