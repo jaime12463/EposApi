ba36e98c9ccd173b1892326fba7119e0a0945661
jimmy <jimmy12463@gmail.com>
2023-06-27 11:51:45 -0600

    Se agrego una nueva clase Discount

---
+++ EposApi/Models/Discount.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EposApi.Models
+{
+    public  class Discount
+    {
+        [JsonProperty("descripcion")]
+        public string Description { get; set; }
+
+        [JsonProperty("porcentaje")]
+        public long Porcentage { get; set; }
+
+        [JsonProperty("subfamilia")]
+        public long SubFamily { get; set; }
+    }
+}
